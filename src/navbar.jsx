e221920f465a6753c93a6426a46fe9969ab941ca
Priyanka singh <160533231+PRIYANKA22BSC@users.noreply.github.com>
2024-12-19 19:08:14 +0530

    first commit

---
+++ src/navbar.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+
+function Navbar() {
+    
+  
+    return (
+      <>
+        <div id="navbar-container">
+            <div id="nav-logo">Logo</div>
+            <div id="navlinks-box">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+            </div>
+        </div>
+      </>
+    )
+  }
+  
+  export default Navbar
